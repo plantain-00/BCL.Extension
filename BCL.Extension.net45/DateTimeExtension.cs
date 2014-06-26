809bf02c14a612308fd042ff766c60bfa8cba6f1
plantain-00 <y.yao@hugegis.com>
2014-06-26 22:30:47 +0800

    first edition

---
+++ BCL.Extension.net45/DateTimeExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BCL.Extension
+{
+    /// <summary>
+    ///     DateTime结构的扩展
+    /// </summary>
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        ///     Unix时间戳转换为DateTime
+        /// </summary>
+        /// <param name="secondsFrom19700101"></param>
+        /// <returns></returns>
+        public static DateTime ToDateTime(this int secondsFrom19700101)
+        {
+            return new DateTime(1970, 1, 1).AddSeconds(secondsFrom19700101);
+        }
+        /// <summary>
+        ///     DateTime转换为Unix时间戳
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static int ToInt32(this DateTime dateTime)
+        {
+            return Convert.ToInt32((dateTime - new DateTime(1970, 1, 1)).TotalSeconds);
+        }
+    }
+}
---
+++ BCL.Extension.net45/ExceptionExtension.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace BCL.Extension
+{
+    /// <summary>
+    ///     Exception类的扩展
+    /// </summary>
+    public static class ExceptionExtension
+    {
+        /// <summary>
+        ///     否则抛出异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"></exception>
+        public static void OrThrows(this bool condition, string message = null)
+        {
+            if (!condition)
+            {
+                throw new Exception(message ?? "");
+            }
+        }
+        /// <summary>
+        ///     则抛出异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"></exception>
+        public static void ThenThrows(this bool condition, string message = null)
+        {
+            if (condition)
+            {
+                throw new Exception(message ?? "");
+            }
+        }
+        /// <summary>
+        ///     如果condition为True，则抛出异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <exception cref="Exception"></exception>
+        public static void ThrowIf(bool condition)
+        {
+            if (condition)
+            {
+                throw new Exception();
+            }
+        }
+        /// <summary>
+        ///     如果condition为True，则抛出异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message"></param>
+        /// <exception cref="Exception"></exception>
+        public static void ThrowIf(bool condition, string message)
+        {
+            if (message == null)
+            {
+                throw new Exception("message");
+            }
+            if (condition)
+            {
+                throw new Exception(message);
+            }
+        }
+        /// <summary>
+        ///     如果condition为True，则抛出参数异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void ThrowIfArgument(bool condition)
+        {
+            if (condition)
+            {
+                throw new ArgumentException();
+            }
+        }
+        /// <summary>
+        ///     如果condition为True，则抛出参数异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <param name="message"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void ThrowIfArgument(bool condition, string message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentException("message");
+            }
+            if (condition)
+            {
+                throw new ArgumentException(message);
+            }
+        }
+        /// <summary>
+        ///     如果condition为null，则抛出参数为空异常
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void ThrowIfArgumentIsNull(this object condition)
+        {
+            if (condition == null)
+            {
+                throw new ArgumentNullException("condition");
+            }
+        }
+    }
+}

0287a52b5dc7c17f6c5b0dd06b3dc8747cb595b5
plantain-00 <yaoyao12306@163.com>
2014-07-06 22:36:16 +0800

    add queryableextension

---
+++ BCL.Extension.net45/QueryableExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace BCL.Extension
+{
+    /// <summary>
+    ///     IQueryable的扩展
+    /// </summary>
+    public static class QueryableExtension
+    {
+        /// <summary>
+        ///     按查询条件过滤
+        /// </summary>
+        /// <param name="set"></param>
+        /// <param name="condition"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IQueryable<T> Choose<T>(this IQueryable<T> set, Func<IQueryable<T>, IQueryable<T>> condition)
+        {
+            return condition(set);
+        }
+    }
+}

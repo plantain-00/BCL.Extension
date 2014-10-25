248256de22787a64b6243363d5abc61ec9ce714d
yaoyao <yaoyao12306@163.com>
2014-10-25 21:42:05 +0800

    upgrade to v 1.0.

--- BCL.Extension.Example.net35/Program.cs
+++ BCL.Extension.Example.net35/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 
 namespace BCL.Extension.Example.net35
@@ -7,27 +8,44 @@ namespace BCL.Extension.Example.net35
     {
         private static void Main(string[] args)
         {
-            (args.Length == 0).OrThrow();
-            (args.Length != 0).ThenThrow("length equals 0");
-            args.IsNullThenThrow();
-            var time = DateTime.Now.ToInt32();
-            var date = 1403980977.ToDateTime();
-            var datatable = new DataTable();
-            var column1 = new DataColumn
-                          {
-                              DataType = Type.GetType("System.Int32"),
-                              ColumnName = "Age"
-                          };
-            datatable.Columns.Add(column1);
-            var column2 = new DataColumn
-                          {
-                              DataType = Type.GetType("System.String"),
-                              ColumnName = "Name"
-                          };
-            datatable.Columns.Add(column2);
-            datatable.Rows.Add(1, "a");
-            datatable.Rows.Add(2, "b");
-            var list = datatable.ToList<TestModel>();
+            //(args.Length == 0).OrThrow();
+            //(args.Length != 0).ThenThrow("length equals 0");
+            //args.IsNullThenThrow();
+            //var time = DateTime.Now.ToInt32();
+            //var date = 1403980977.ToDateTime();
+            //var datatable = new DataTable();
+            //var column1 = new DataColumn
+            //              {
+            //                  DataType = Type.GetType("System.Int32"),
+            //                  ColumnName = "Age"
+            //              };
+            //datatable.Columns.Add(column1);
+            //var column2 = new DataColumn
+            //              {
+            //                  DataType = Type.GetType("System.String"),
+            //                  ColumnName = "Name"
+            //              };
+            //datatable.Columns.Add(column2);
+            //datatable.Rows.Add(1, "a");
+            //datatable.Rows.Add(2, "b");
+            //var list = datatable.ToList<TestModel>();
+
+            var testModels = new List<TestModel>
+                             {
+                                 new TestModel
+                                 {
+                                     Name = "a",
+                                     Age = 1
+                                 },
+                                 new TestModel
+                                 {
+                                     Name = "b",
+                                     Age = 2
+                                 }
+                             };
+            var s = XmlConverter.Serialize(testModels);
+            var t = XmlConverter.Deserialize<List<TestModel>>(s);
+            Console.Read();
         }
     }
 }
---
+++ BCL.Extension.net45/XmlConverter.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace BCL.Extension
+{
+    /// <summary>
+    ///     对象和Xml的序列化和反序列化
+    /// </summary>
+    public static class XmlConverter
+    {
+        /// <summary>
+        ///     从Xml文件反序列化出对象
+        /// </summary>
+        /// <param name="path"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T DeserializeFile<T>(string path)
+        {
+            return (T) new XmlSerializer(typeof (T)).Deserialize(new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite));
+        }
+
+        /// <summary>
+        ///     序列化至Xml文件
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="path"></param>
+        public static void SerializeFile(object obj, string path)
+        {
+            new XmlSerializer(obj.GetType()).Serialize(new FileStream(path, FileMode.Create, FileAccess.ReadWrite), obj);
+        }
+
+        /// <summary>
+        ///     从字符串反序列化出对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string xml)
+        {
+            using (var reader = new StringReader(xml))
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                return (T) serializer.Deserialize(reader);
+            }
+        }
+
+        /// <summary>
+        ///     序列化成字符串
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string Serialize(object obj)
+        {
+            //using (var stream = new MemoryStream())
+            //{
+            //    var serializer = new XmlSerializer(obj.GetType());
+            //    serializer.Serialize(stream, obj);
+            //    using (var sr = new StreamReader(stream))
+            //    {
+            //        return sr.ReadToEnd();
+            //    }
+            //}
+            using (var writer = new StringWriter())
+            {
+                var serializer = new XmlSerializer(obj.GetType());
+                serializer.Serialize(writer, obj);
+                return writer.ToString();
+            }
+        }
+    }
+}
